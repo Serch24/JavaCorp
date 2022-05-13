2dcf120be4499bf1ae1b13b9cb80497af378220a
serch <serchhhh>
2022-05-13 10:49:36 +0200

    Ya se puede añadir Jlabels al Jpanel dinamicamente, había que configurar el layout correctamente

--- src/main/java/interfaz/Loged.java
+++ src/main/java/interfaz/Loged.java
@@ -1,17 +1,24 @@
 package interfaz;
 
+import java.awt.Image;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.ObjectInputStream;
+import java.time.LocalDate;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
 import poo.javacorp.CategoriaProductos;
+import poo.javacorp.Productos;
 import poo.javacorp.Usuarios;
 
 
 public class Loged extends javax.swing.JFrame {
 	Usuarios usuario;
+	String imagenes = "src/main/java/imagenes/";
+
 	
 	public Loged(Usuarios usuarioLoged) {
 		initComponents();
@@ -50,6 +57,7 @@ public class Loged extends javax.swing.JFrame {
                 buscarCategorias = new javax.swing.JComboBox<>();
                 barraBusqueda = new javax.swing.JTextField();
                 BotonBusqueda = new javax.swing.JButton();
+                panelTest = new javax.swing.JPanel();
 
                 setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -63,6 +71,13 @@ public class Loged extends javax.swing.JFrame {
                 });
 
                 BotonBusqueda.setText("Buscar");
+                BotonBusqueda.addActionListener(new java.awt.event.ActionListener() {
+                        public void actionPerformed(java.awt.event.ActionEvent evt) {
+                                BotonBusquedaActionPerformed(evt);
+                        }
+                });
+
+                panelTest.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
 
                 javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
                 getContentPane().setLayout(layout);
@@ -71,13 +86,17 @@ public class Loged extends javax.swing.JFrame {
                         .addGroup(layout.createSequentialGroup()
                                 .addGap(26, 26, 26)
                                 .addComponent(bienv)
-                                .addGap(38, 38, 38)
-                                .addComponent(buscarCategorias, javax.swing.GroupLayout.PREFERRED_SIZE, 128, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 56, Short.MAX_VALUE)
+                                .addGap(68, 68, 68)
+                                .addComponent(buscarCategorias, javax.swing.GroupLayout.PREFERRED_SIZE, 174, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 33, Short.MAX_VALUE)
                                 .addComponent(barraBusqueda, javax.swing.GroupLayout.PREFERRED_SIZE, 231, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addGap(37, 37, 37)
+                                .addGap(26, 26, 26)
                                 .addComponent(BotonBusqueda, javax.swing.GroupLayout.PREFERRED_SIZE, 78, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addGap(76, 76, 76))
+                                .addGap(34, 34, 34))
+                        .addGroup(layout.createSequentialGroup()
+                                .addGap(42, 42, 42)
+                                .addComponent(panelTest, javax.swing.GroupLayout.PREFERRED_SIZE, 644, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                 );
                 layout.setVerticalGroup(
                         layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -88,7 +107,9 @@ public class Loged extends javax.swing.JFrame {
                                         .addComponent(buscarCategorias, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                                         .addComponent(barraBusqueda, javax.swing.GroupLayout.PREFERRED_SIZE, 26, javax.swing.GroupLayout.PREFERRED_SIZE)
                                         .addComponent(BotonBusqueda))
-                                .addContainerGap(399, Short.MAX_VALUE))
+                                .addGap(42, 42, 42)
+                                .addComponent(panelTest, javax.swing.GroupLayout.PREFERRED_SIZE, 316, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addContainerGap(41, Short.MAX_VALUE))
                 );
 
                 pack();
@@ -98,11 +119,27 @@ public class Loged extends javax.swing.JFrame {
                 // TODO add your handling code here:
         }//GEN-LAST:event_buscarCategoriasActionPerformed
 
+        private void BotonBusquedaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BotonBusquedaActionPerformed
+               
+		ImageIcon i = new ImageIcon(imagenes + "azul.png");
+		Image aux = i.getImage().getScaledInstance(120, 120, java.awt.Image.SCALE_SMOOTH);
+		i = new ImageIcon(aux);
+		JLabel test = new JLabel(i);
+		//falta darle un size a las imagenes
+		System.out.println(test.getLocation());
+		System.out.println(panelTest.getLocation());
+		panelTest.add(test);
+		// ya funciona añadir dinamicamente un Jlabel!!
+		pack();
+		
+        }//GEN-LAST:event_BotonBusquedaActionPerformed
+
 
         // Variables declaration - do not modify//GEN-BEGIN:variables
         private javax.swing.JButton BotonBusqueda;
         private javax.swing.JTextField barraBusqueda;
         private javax.swing.JLabel bienv;
         private javax.swing.JComboBox<String> buscarCategorias;
+        private javax.swing.JPanel panelTest;
         // End of variables declaration//GEN-END:variables
 }
